48d64f42a6bb38eb5c1be5ff447e87610c938409
Fran Canete <francanetedev@gmail.com>
2021-10-05 15:18:38 +0100

    Add handlePlay functio to the Play button

--- src/App.js
+++ src/App.js
@@ -6,30 +6,38 @@ function App() {
   const [player1, setPlayer1] = useState("");
   const [player2, setPlayer2] = useState("");
 
-  useEffect(() => {
+  const randomPlayer = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  };
+
+  const handlePlay = () => {
+    const people1 = randomPlayer(1, 80);
+
     axios
-      .get("https://swapi.dev/api/people/1")
+      .get(`https://swapi.dev/api/people/${people1}`)
       .then((res) => {
         setPlayer1(res.data);
       })
       .catch((error) => alert("there is an error fetching your data"));
-  }, []);
 
-  useEffect(() => {
+    const people2 = randomPlayer(1, 80);
+
     axios
-      .get("https://swapi.dev/api/people/2")
+      .get(`https://swapi.dev/api/people/${people2}`)
       .then((res) => {
         setPlayer2(res.data);
       })
       .catch((error) => alert("there is an error fetching your data"));
-  }, []);
-
-  console.log(player1.mass);
+  };
 
   return (
     <div className="container">
       <div className="text-center">
-        <button type="button" class="btn btn-primary btn-lg m-3 ">
+        <button
+          type="button"
+          className="btn btn-primary btn-lg m-3"
+          onClick={() => handlePlay()}
+        >
           Play!
         </button>
       </div>
@@ -39,18 +47,18 @@ function App() {
           <div className="card">
             <div className="card-body">
               <h5 className="card-title">{player1.name}</h5>
-              <p className="card-text">{`${player1.name} has a mass power of ${player1.mass}.`}</p>
+              <p className="card-text">{`${player1.name} has a height of ${player1.height}.`}</p>
             </div>
-            <span class="badge bg-success">Success</span>
+            <span className="badge bg-success">Success</span>
           </div>
         </div>
         <div className="col">
           <div className="card">
             <div className="card-body">
               <h5 className="card-title">{player2.name}</h5>
-              <p className="card-text">{`${player2.name} has a mass power of ${player2.mass}.`}</p>
+              <p className="card-text">{`${player2.name} has a height of ${player2.height}.`}</p>
             </div>
-            <span class="badge bg-danger">Danger</span>
+            <span className="badge bg-danger">Danger</span>
           </div>
         </div>
       </div>
