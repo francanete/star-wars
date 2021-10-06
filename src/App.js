d03e46fee6f4f8e1e7f961cd9d45e77777ca2004
Fran Canete <francanetedev@gmail.com>
2021-10-06 13:07:26 +0100

    Add badge to winner/loser

--- src/App.js
+++ src/App.js
@@ -5,9 +5,22 @@ import axios from "axios";
 function App() {
   const [player1, setPlayer1] = useState("");
   const [player2, setPlayer2] = useState("");
+  // let [player1IsWinner, setPlayer1IsWinner] = useState(false);
+  // let [player2IsWinner, setPlayer2IsWinner] = useState(false);
 
   const randomPlayer = (min, max) => {
     return Math.floor(Math.random() * (max - min + 1) + min);
+  };
+
+  let Player1IsWinner;
+  let Player2IsWinner;
+
+  const findWinner = (height1, height2) => {
+    if (height1 > height2) {
+      Player1IsWinner = true;
+    } else if (height1 < height2) {
+      Player2IsWinner = true;
+    }
   };
 
   const handlePlay = () => {
@@ -30,6 +43,9 @@ function App() {
       .catch((error) => alert("there is an error fetching your data"));
   };
 
+  findWinner(player1.height, player2.height);
+  console.log(Player1IsWinner);
+
   return (
     <div className="container">
       <div className="text-center">
@@ -49,7 +65,11 @@ function App() {
               <h5 className="card-title">{player1.name}</h5>
               <p className="card-text">{`${player1.name} has a height of ${player1.height}.`}</p>
             </div>
-            <span className="badge bg-success">Success</span>
+            {Player1IsWinner ? (
+              <span className="badge bg-success">Winner</span>
+            ) : (
+              <span className="badge bg-danger">Loser</span>
+            )}
           </div>
         </div>
         <div className="col">
@@ -58,7 +78,11 @@ function App() {
               <h5 className="card-title">{player2.name}</h5>
               <p className="card-text">{`${player2.name} has a height of ${player2.height}.`}</p>
             </div>
-            <span className="badge bg-danger">Danger</span>
+            {Player2IsWinner ? (
+              <span className="badge bg-success">Winner</span>
+            ) : (
+              <span className="badge bg-danger">Loser</span>
+            )}
           </div>
         </div>
       </div>
